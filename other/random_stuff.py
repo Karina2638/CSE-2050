d4dfeef190b177806d9cad44c8200cf4f75300b2
Karina2638 <karina.jadia@gmail.com>
2023-07-31 13:03:41 -0400

    deleted:    other/game_day_one.py 	deleted:    other/guessing_game.py 	deleted:    other/idk.py 	deleted:    other/random_stuff.py 	deleted:    other/word_guessing_game.py

--- other/random_stuff.py
+++
@@ -1,9 +0,0 @@
-work = 40
-salary = 32
-year = 52
-
-yearly = work*salary*year
-
-print(f'yearly salary: ${yearly}')
-print(f'monthly salary: ${yearly//12}')
-print(f'biweekly salary: ${yearly//(12*2)}')
